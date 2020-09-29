5c1a0f17e6d2ab4802bfddbb4f5e1846545901dc
Ekenzy-101 <ekeneonyekaba@gmail.com>
2020-09-29 05:02:05 +0100

    Updated validations

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -4,9 +4,13 @@ import { TOKEN_KEY } from "../utils/constant";
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    "Access-Control-Allow-Origin": "*",
+    common: {
+      "Access-Control-Allow-Origin": "*",
+    },
   },
 });
+
+inst;
 
 instance.defaults.headers.common["x-auth-token"] = localStorage.getItem(
   TOKEN_KEY
